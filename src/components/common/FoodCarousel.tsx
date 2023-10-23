7a82592b8073ec0a22a6bffade28c2336a0b636a
Louis Kwok <87352246+LouisKwok92334@users.noreply.github.com>
2023-10-23 10:14:35 +0800

    Update FoodCarousel.tsx

--- src/components/common/FoodCarousel.tsx
+++ src/components/common/FoodCarousel.tsx
@@ -11,19 +11,16 @@ const items = [
   {
     src: 'https://picsum.photos/id/123/1200/400',
     altText: 'Slide 1',
-    caption: 'Slide 1',
     key: 1,
   },
   {
     src: 'https://picsum.photos/id/456/1200/400',
     altText: 'Slide 2',
-    caption: 'Slide 2',
     key: 2,
   },
   {
     src: 'https://picsum.photos/id/678/1200/400',
     altText: 'Slide 3',
-    caption: 'Slide 3',
     key: 3,
   },
 ];
@@ -57,10 +54,6 @@ export function FoodCarousel(args: Record<string, unknown>) {
         key={item.src}
       >
         <img src={item.src} alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        />
       </CarouselItem>
     );
   });
