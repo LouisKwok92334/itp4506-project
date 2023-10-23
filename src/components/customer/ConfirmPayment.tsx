5e7171fb0cc90e04510692a13456dbe28e6e9c64
boscochuen011 <Bosco.chuen@gmail.com>
2023-10-23 21:31:39 +0800

    update Confirm Payment

--- src/components/customer/ConfirmPayment.tsx
+++ src/components/customer/ConfirmPayment.tsx
@@ -1,10 +1,37 @@
-import './ConfirmPayment.css'
+import React from "react";
+import "./ConfirmPayment.css";
+import { useNavigate } from "react-router-dom";
 
 export function ConfirmPayment() {
-    return (
-      <div className="ConfirmPayment">
-        
+  const navigate = useNavigate();
+
+  const handlePaymentConfirmation = () => {
+    navigate("/confirm-order");
+  };
+
+  return (
+    <div className="ConfirmPayment">
+      <div>
+        <h2 className="bold-text">Confirm Order</h2>
       </div>
-    );
-  }
-  
+      <div className="order-details-container">
+        <div className="square"></div>
+      </div>
+      <div className="payment-header">
+  <h2 className="bold-text">Confirm Payment</h2>
+  <button className="edit-button" >
+    Edit
+  </button>
+</div>
+
+      <div className="payment-details-container">
+        <div className="square"></div>
+      </div>
+      <div className="button-container">
+        <button className="confirm-button" onClick={handlePaymentConfirmation}>
+          Confirm Payment
+        </button>
+      </div>
+    </div>
+  );
+}
