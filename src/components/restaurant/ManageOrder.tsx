e4742811ea7d7185aa4557c2dfa0b74ee5b44c9c
LouisKwok92334 <87352246+LouisKwok92334@users.noreply.github.com>
2023-11-30 20:18:30 +0800

    update manage order

--- src/components/restaurant/ManageOrder.tsx
+++ src/components/restaurant/ManageOrder.tsx
@@ -1,19 +1,52 @@
 import React, { useState } from 'react';
 import { Table, Button, Modal, Form } from 'react-bootstrap';
+import ordersData from '../../json/order.json';
+
+type OrderItemType = {
+  name: string;
+  quantity: number;
+};
 
 type OrderType = {
   id: number;
   customer: string;
-  items: string;
+  items: OrderItemType[];
   total: string;
-  status: string;
+  status: 'Pending' | 'Preparing' | 'Ready for Delivery' | 'Completed' | 'Rejected';
 };
 
 const mockOrders: OrderType[] = [
-  { id: 1, customer: 'John Doe', items: 'Pizza, Soda', total: '20.00', status: 'Preparing' },
-  { id: 2, customer: 'Jane Smith', items: 'Burger, Water', total: '15.00', status: 'Pending' },
+  { 
+    id: 1, 
+    customer: 'John Doe', 
+    items: [
+      { name: 'Pizza', quantity: 1 },
+      { name: 'Soda', quantity: 2 }
+    ], 
+    total: '20.00', 
+    status: 'Preparing' 
+  },
+  { 
+    id: 2, 
+    customer: 'Jane Smith', 
+    items: [
+      { name: 'Burger', quantity: 1 },
+      { name: 'Water', quantity: 1 }
+    ], 
+    total: '15.00', 
+    status: 'Pending' 
+  },
   // 更多订单...
 ];
+
+
+const statusColors: { [key in OrderType['status']]: string } = {
+  Pending: 'warning',
+  Preparing: 'info',
+  'Ready for Delivery': 'primary',
+  Completed: 'success',
+  Rejected: 'danger'
+};
 
 export function ManageOrder() {
   const [orders, setOrders] = useState<OrderType[]>(mockOrders);
@@ -21,9 +54,27 @@ export function ManageOrder() {
   const [showModal, setShowModal] = useState(false);
 
   const handleStatusChange = (orderId: number, newStatus: string) => {
+    const validStatuses = ['Pending', 'Preparing', 'Ready for Delivery', 'Completed', 'Rejected'];
+    if (!validStatuses.includes(newStatus)) {
+      console.error('Invalid status:', newStatus);
+      return;
+    }
+  
     setOrders(orders.map(order => 
-      order.id === orderId ? { ...order, status: newStatus } : order
+      order.id === orderId ? { ...order, status: newStatus as OrderType['status'] } : order
     ));
+  };  
+
+  const getOrderItemsString = (items: OrderItemType[]): string => {
+    return items.map(item => `${item.name} x${item.quantity}`).join(', ');
+  };
+
+  const handleAcceptOrder = (orderId: number) => {
+    // 处理接受订单逻辑
+  };
+
+  const handleRejectOrder = (orderId: number) => {
+    // 处理拒绝订单逻辑
   };
 
   const openOrderModal = (order: OrderType) => {
@@ -53,11 +104,23 @@ export function ManageOrder() {
             <tr key={order.id}>
               <td>{order.id}</td>
               <td>{order.customer}</td>
-              <td>{order.items}</td>
+              <td>{getOrderItemsString(order.items)}</td>
               <td>${order.total}</td>
-              <td>{order.status}</td>
               <td>
-                <Button variant="primary" onClick={() => openOrderModal(order)}>Manage</Button>
+                <span className={`badge bg-${statusColors[order.status]}`}>
+                {order.status}
+                </span>
+              </td>
+              <td>
+                {order.status === 'Pending' && (
+                  <>
+                    <Button variant="success" onClick={() => handleAcceptOrder(order.id)}>Accept</Button>
+                    <Button variant="danger" onClick={() => handleRejectOrder(order.id)}>Reject</Button>
+                  </>
+                )}
+                {order.status !== 'Pending' && (
+                  <Button variant="primary" onClick={() => openOrderModal(order)}>Manage</Button>
+                )}
               </td>
             </tr>
           ))}
@@ -78,7 +141,11 @@ export function ManageOrder() {
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label>Order Items</Form.Label>
-                <Form.Control type="text" readOnly defaultValue={selectedOrder.items} />
+                <ul>
+                  {selectedOrder.items.map((item, index) => (
+                    <li key={index}>{item.name} x{item.quantity}</li>
+                  ))}
+                </ul>
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label>Total Price</Form.Label>
